1e9e9cdb4aa36767e841dca3ed0704e7eeb176f3
kimcabigas <imkc0923@gmail.com>
2025-11-16 15:06:38 +1100

    committing changes

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 console.log("Portfolio Loaded");
+
+// DARK MODE TOGGLE
+const toggleBtn = document.getElementById("darkModeToggle");
+const body = document.body;
+
+// Load saved mode
+if (localStorage.getItem("darkMode") === "enabled") {
+    body.classList.add("dark-mode");
+    toggleBtn.textContent = "☀️";
+}
+
+toggleBtn.addEventListener("click", () => {
+    body.classList.toggle("dark-mode");
+
+    if (body.classList.contains("dark-mode")) {
+        toggleBtn.textContent = "☀️";
+        localStorage.setItem("darkMode", "enabled");
+    } else {
+        toggleBtn.textContent = "🌙";
+        localStorage.setItem("darkMode", "disabled");
+    }
+});
